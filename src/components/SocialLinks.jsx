9bb994c2b2150e765c6c57253f1862c3a5d8adaf
Rubaina <rtau0237@uni.sydney.edu.au>
2024-02-07 17:24:54 +0600

    social links added

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -1,33 +1,71 @@
 import React from 'react'
 import {FaGithub, FaLinkedin} from 'react-icons/fa'
-// import {HiOutlineMail} from 'react-icons/hi'
-// import {BsFillPersonLinesFill} from 'react-icons/bs'
+import {HiOutlineMail} from 'react-icons/hi'
+import {BsFillPersonLinesFill} from 'react-icons/bs'
 
 const SocialLinks = () => {
 
-    // const links = [
-    //     {
-    //         id:1,
-    //         child: ()
-    //     }
-    // ]
-  return (
-    <div className='flex flex-col top-[35%] left-0 fixed'>
-        {/* unordered list */}
-        <ul> 
-            {/* list item element */}
-            <li className='flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-600'>
-                {/* anchor link element, we use to create hyperlink element */}
-                <a href='www.google.com' className='flex justify-between items-center w-full text-white'>
-                    {/* <></> are React fragments to group multiple elements without adding extra node to the DOM */}
+    const links = [
+        {
+            id:1,
+            child: (
                     <>
                     LinkedIn <FaLinkedin size={30}/> 
                     </>
-                </a>
+            ),
+            href: 'https://linkedin.com',
+            style: 'rounded-tr-md'
+        },
+        {
+            id:2,
+            child: (
+                    <>
+                    GitHub <FaGithub size={30}/> 
+                    </>
+            ),
+            href: 'https://github.com',
+        },
+        {
+            id:3,
+            child: (
+                    <>
+                    Mail <HiOutlineMail size={30}/> 
+                    </>
+            ),
+            href: 'hmailto:foo@gmail.com',
+        },
+        {
+            id:4,
+            child: (
+                    <>
+                    Resume <BsFillPersonLinesFill size={30}/> 
+                    </>
+            ),
+            href: '/resume.pdf',
+            style: 'rounded-br-md',
+            download: true
+        },
+    ]
+  return (
+    <div className='flex flex-col top-[35%] left-0 fixed'>
+        <ul> 
+            {links.map(({id, child, href, style, download}) => (
+                <li key ={id} className={
+                    `flex justify-between items-center w-40 h-14 px-4 ml-[-100px] hover:ml-[-10px] hover:rounded-md duration-300 bg-gray-600 ${style}`
+                }>
+            {/* anchor link element, we use to create hyperlink element */}
+            <a href={href} className='flex justify-between items-center w-full text-white'
+            download={download}
+            // open the tab in a new link
+            target='_blank'
+            rel='noreferrer'> 
+            {child}
+            </a>
             </li>
+            ))}
         </ul>
     </div>
-  )
-}
+  );
+};
 
 export default SocialLinks
